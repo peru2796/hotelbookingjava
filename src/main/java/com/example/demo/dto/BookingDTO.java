9e4e1e51b8127537bc65080cdf68b8ef5e10c928
p.sankaramahalingam <perumalrajsnrp@gmail.com>
2025-12-26 06:54:56 +0530

    added client and booking in same endpoint

---
+++ src/main/java/com/example/demo/dto/BookingDTO.java
@@ -0,0 +1,57 @@
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BookingDTO {
+
+    private Long bookingId;
+    private Long roomId;
+    private Long roomNumber;
+    private Long clientId;
+    private Double totalAmount;
+    private Double amountPaid;
+    private Double amountRemaining;
+    private String paymentType;
+    private Integer transactionStatus;
+
+    private Timestamp bookedDts;
+
+    private Timestamp arrivalDts;
+
+    private Timestamp checkinDts;
+    private Timestamp checkoutDts;
+    private Timestamp cancelledtDts;
+    private String comments;
+
+    private String firstName;
+    private String lastName;
+    private String initial;
+    private String email;
+
+
+    private String mobileNumber;
+
+
+    private String idNumber;
+
+    private String Address1;
+    private String Address2;
+
+    private String city;
+
+    private String state;
+
+    private Integer pinCode;
+
+    private byte[] image;
+
+    private String fileName;
+
+    private String fileType;
+}
